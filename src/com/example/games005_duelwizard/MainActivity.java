6de120fbfee1b59383e281bc94ece966070686af
mori_yu <yuuto0609@gmail.com>
2014-08-21 22:42:53 +0900

    画面遷移を実装

--- src/com/example/games005_duelwizard/MainActivity.java
+++ src/com/example/games005_duelwizard/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.games005_duelwizard;
 
+import com.example.games005_duelwizard.StartFragment.OnBattleStartListener;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements OnBattleStartListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +19,19 @@ public class MainActivity extends FragmentActivity {
 		FragmentManager fm = getSupportFragmentManager();
 		FragmentTransaction ft = fm.beginTransaction();
 
-		BattleFragment fragment = new BattleFragment();
-		ft.add(R.id.MainActivity_LinearLayout, fragment);
+		StartFragment mStartFragment = new StartFragment();
+		mStartFragment.setOnBattleStartListener(this);
+		ft.add(R.id.MainActivity_LinearLayout, mStartFragment);
 		ft.commit();
 	}
 
-	// @Override
-	// public boolean onCreateOptionsMenu(Menu menu) {
-	// // Inflate the menu; this adds items to the action bar if it is present.
-	// getMenuInflater().inflate(R.menu.main, menu);
-	// return true;
-	// }
+	@Override
+	public void onGameStart() {
+		FragmentManager fm = getSupportFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		BattleFragment mBattleFragment = new BattleFragment();
+		ft.replace(R.id.MainActivity_LinearLayout, mBattleFragment);
+		ft.commit();
+	}
 
 }
--- src/com/example/games005_duelwizard/StartFragment.java
+++ src/com/example/games005_duelwizard/StartFragment.java
@@ -1,7 +1,56 @@
 package com.example.games005_duelwizard;
 
+import java.util.EventListener;
+
+import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class StartFragment extends Fragment {
+public class StartFragment extends Fragment implements OnClickListener {
 
+	OnBattleStartListener mListener;
+	Button mButtonGameStart;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_start, container, false);
+		Context context = getActivity();
+
+		setViews(v);
+
+		return v;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+			case R.id.StartFragment_button_start:
+				mListener.onGameStart();
+				break;
+
+			default:
+				break;
+		}
+
+	}
+
+	private void setViews(View v) {
+		mButtonGameStart = (Button) v.findViewById(R.id.StartFragment_button_start);
+
+		mButtonGameStart.setOnClickListener(this);
+	}
+
+	public void setOnBattleStartListener(OnBattleStartListener listener) {
+		this.mListener = listener;
+	}
+
+	public interface OnBattleStartListener extends EventListener {
+		void onGameStart();
+	}
 }
